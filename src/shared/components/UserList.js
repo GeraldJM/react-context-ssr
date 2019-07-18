2822f3498064ee613f48668d1f0e627a2df671b0
Gerald Matthews <gerald.matthews2014@gmail.com>
2019-07-18 18:52:03 -0400

    Revert "Replaced useContext with Consumer"
    This reverts commit 7481e13a86edbb2e2a68912eb4ba3d3821c79221.

--- src/shared/components/UserList.js
+++ src/shared/components/UserList.js
@@ -1,22 +1,18 @@
 import React, { Component, useContext } from 'react';
 import UserContext from './context/UserContext';
 
-const UserList = () => {  
+const UserList = () => {
+  const { users } = useContext(UserContext);
+  
   return (
-    <UserContext.Consumer>
-      {({users}) => {
-        return (
-          <div>
-            <h3>Users List</h3>
-            <ul>
-              {users.map((user, index) => (
-                <li key={index}>{user.lastName}, {user.firstName}</li>
-              ))}
-            </ul>
-          </div>
-        )
-      }}
-    </UserContext.Consumer>
+    <div>
+      <h3>Users List</h3>
+      <ul>
+      {users.map((user, index) => (
+        <li key={index}>{user.lastName}, {user.firstName}</li>
+      ))}
+      </ul>
+    </div>
   )
 }
 
