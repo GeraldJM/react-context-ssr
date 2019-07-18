b34ffdbd72a522ab35523b92b883064c940b4ce0
Gerald Matthews <gerald.matthews2014@gmail.com>
2019-07-18 19:05:35 -0400

    Replaced useContext hook with Consumer

--- src/shared/components/UserList.js
+++ src/shared/components/UserList.js
@@ -1,18 +1,22 @@
-import React, { Component, useContext } from 'react';
+import React from 'react';
 import UserContext from './context/UserContext';
 
 const UserList = () => {
-  const { users } = useContext(UserContext);
-  
   return (
-    <div>
-      <h3>Users List</h3>
-      <ul>
-      {users.map((user, index) => (
-        <li key={index}>{user.lastName}, {user.firstName}</li>
-      ))}
-      </ul>
-    </div>
+    <UserContext.Consumer>
+      {({users}) => {
+        return (
+          <div>
+            <h3>Users List</h3>
+            <ul>
+              {users.map((user, index) => (
+                <li key={index}>{user.lastName}, {user.firstName}</li>
+              ))}
+            </ul>
+          </div>
+        )
+      }}
+    </UserContext.Consumer>
   )
 }
 
